d9fc96f99d63907a6384ee1fce1638688279e021
maximBegar <maxbeg091@mail.ru>
2015-06-11 18:44:27 +0300

    animation

--- common.js
+++ common.js
@@ -21,21 +21,56 @@
     });
 });*/
 
-function overlaySlider(target,overlay){
-    $(target).mouseover(function(){
-        $(overlay).slideDown(300).mouseleave(function(){
-            $(this).slideUp(300);
+function overlaySlider(target,overlay,time){
+    $(overlay).css('margin-top',$(target).height()).addClass('hidden');
+    var flag = true;
+
+    $(target).mouseenter(function(){
+        $(this).mouseleave(function(){
+             if (flag){
+                 $(overlay)
+                     .stop()
+                     .animate({'height':'0px','margin-top':$(target).height()},time)
+                     .finish(function(){
+                       $(overlay).addClass('hidden');
+                 });
+             }
         });
+
+        $(overlay)
+            .removeClass('hidden')
+            .stop()
+            .animate({'height':$(target).height(),'margin-top':'0'},time)
+            .finish(function(){
+               flag = false;
+        });
+
+        $(overlay).mouseleave(function(){
+           $(this)
+               .stop()
+               .animate({'height':'0px','margin-top':$(target).height()},time)
+               .finish(function(){
+                  flag = true;
+                  $(overlay).addClass('hidden');
+           });
+       });
     });
 }
 
 $(function(){
+//
     $('header.mainHeader').load('header.html');
     $('footer').load('footer.html');
     $('.longBtn').load('longBtn.html');
 //TODO include header,footer...
 
-    overlaySlider('.bigPhoto','.overlay');
-    overlaySlider('.smallImgLeft','.overlayLeft');
-    overlaySlider('.smallImgRight','.overlayRight');
+    $('.overlay,.overlayLeft,.overlayRight').mouseenter(function(){
+        $(this).css({'transition':'0.7s','opacity':'1'}).mouseleave(function(){
+            $(this).css('opacity','0.6');
+        });
+    });
+
+    overlaySlider('.servicePhoto.first','.darkOverlay.first',200);
+    overlaySlider('.servicePhoto.sec','.darkOverlay.sec',200);
+    overlaySlider('.servicePhoto.third','.darkOverlay.third',200);
 });
